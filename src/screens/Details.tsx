7c75b26c6833e03b3ca3213c6063f53735a52335
André Kunde <andrelbkunde@gmail.com>
2022-07-21 03:52:33 -0300

    more layout adjusts

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -7,7 +7,7 @@ import {
   CircleWavyCheck,
   Hourglass,
   DesktopTower,
-  Clipboard,
+  ClipboardText,
 } from 'phosphor-react-native';
 
 import { OrderFirebaseDTO } from '../DTOs/OrderFirebaseDTO';
@@ -147,13 +147,13 @@ export function Details() {
           title='equipamento'
           description={`Patrimônio ${order.patrimony}`}
           icon={DesktopTower}
-          footer={order.when}
         />
 
         <CardDetails
           title='descrição do problema'
           description={order.description}
-          icon={Clipboard}
+          icon={ClipboardText}
+          footer={`Registrado em ${order.when}`}
         />
 
         <CardDetails
